13fd147a2ee43ceebdae00102f19172c4bed3708
bipul dawadi <bipul>
2023-02-20 20:49:35 +0545

    start

---
+++ userapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def signup(request):
+    return render(request,'signup.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+
+def logout(request):
+    return render(request,'logout.html')
+
+
+
+
