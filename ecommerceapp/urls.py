92b732a99ad72f004b066ddba91a598681b74cb4
bipul dawadi <bipul>
2023-02-20 12:57:38 +0545

    navbar  completed

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import include,path
 from ecommerceapp import views
 
 urlpatterns=[
-   path('',views.index,name='index')
+   path('',views.home,name='home'),
+   path('about',views.about,name='about'),
+   path('contact',views.contact,name='contact'),
 ]
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'base.html',{'title': 'welcome to django ecommerce.com'})
+def home(request):
+    return render(request, 'home.html',{'title': 'welcome to django ecommerce.com'})
+def contact(request):
+    
+    return render(request, 'contact.html',{'title': 'welcome to django ecommerce.com'})
+def about(request):
+    
+    return render(request, 'about.html',{'title': 'welcome to django ecommerce.com'})
